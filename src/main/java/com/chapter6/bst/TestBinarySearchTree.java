58b786cc6e787b1995241747427bc3850d3a4411
Yan An <yananzx@yeah.net>
2018-04-12 00:32:52 +0800

    update for BST

--- src/main/java/com/chapter6/bst/TestBinarySearchTree.java
+++ src/main/java/com/chapter6/bst/TestBinarySearchTree.java
@@ -10,14 +10,23 @@ public class TestBinarySearchTree {
 
 	public static void main(String[] args) {
 		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
-		for(int i = 1; i<101 ; i++){
+		/*for(int i = 1; i<101 ; i++){
 			bst.insert(i);
 		}
 		System.out.println("max is:"+bst.findMax());
 		System.out.println("min is:"+bst.findMin());
 		System.out.println("in order iterate:");
 		bst.inorder(bst.root);
-		System.out.println("\n二叉搜索树高度"+bst.countHeight(bst.root));
+		System.out.println("\n二叉搜索树高度"+bst.countHeight(bst.root));*/
+		bst.insert(6);
+		bst.insert(2);
+		bst.insert(8);
+		bst.insert(1);
+		bst.insert(4);
+		bst.insert(3);
+		bst.inorder(bst.root);
+		bst.delete(2);
+		bst.inorder(bst.root);
 	}
 
 }
