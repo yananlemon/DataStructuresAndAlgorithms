8b9bc07dbda05678a2eea3ebc1d87e4bb12082dd
yanan <yananzx@yeah.net>
2018-04-24 22:50:53 +0800

    第十六题

---
+++ src/main/java/com/chapter6/SixteenthQuesion.java
@@ -0,0 +1,22 @@
+package com.chapter6;
+
+import com.chapter6.navl.AvlTree;
+
+/**
+ * <p>SixteenthQuesion</p>
+ * <p>第十六题</p>
+ * @author yanan
+ * @date 2018年4月24日
+ */
+public class SixteenthQuesion {
+
+	public static void main(String[] args) {
+		AvlTree<Character> tree = new AvlTree<Character>();
+		for (char i = 'a'; i <= 'j'; i++) {
+			tree.insert(i);
+		}
+		System.out.println("created!");
+		
+	}
+
+}
