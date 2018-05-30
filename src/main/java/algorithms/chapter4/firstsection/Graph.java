e00553050ea1cc000b1cea53398109cd884319fa
aaron.yan <aaron.yan@analyticservice.net>
2018-05-30 17:19:38 +0800

    add image for chapter4.1.2

---
+++ src/main/java/algorithms/chapter4/firstsection/Graph.java
@@ -0,0 +1,10 @@
+package algorithms.chapter4.firstsection;
+
+public class Graph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/chapter8/dfs/Graph.java
+++ src/main/java/com/chapter8/dfs/Graph.java
@@ -94,13 +94,31 @@ public class Graph {
     }
 
 	public static void main(String[] args) {
-		Graph g = new Graph(5);
+		/*Graph g = new Graph(5);
 		g.addEdge(0,1);
 		g.addEdge(0,3);
 		g.addEdge(1,2);
 		g.addEdge(1,4);
 		g.addEdge(2,3);
-		g.addEdge(2,4);
+		g.addEdge(2,4);*/
+		Graph g = new Graph(12);
+		g.addEdge(8,4);
+		g.addEdge(2,3);
+		g.addEdge(1,11);
+		g.addEdge(0,6);
+		g.addEdge(3,6);
+		g.addEdge(10,3);
+		g.addEdge(7,11);
+		g.addEdge(7,8);
+		g.addEdge(11,8);
+		g.addEdge(2,0);
+		g.addEdge(6,2);
+		g.addEdge(5,2);
+		g.addEdge(5,10);
+		g.addEdge(5,0);
+		g.addEdge(8,1);
+		g.addEdge(4,1);
+		
 		System.out.println(g);
 	}
 
