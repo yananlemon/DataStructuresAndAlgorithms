135aab9003f34be8ac41e73d6034416e705dca75
yanan <yananzx@yeah.net>
2018-06-04 21:21:14 +0800

    add test code

--- src/main/java/algorithms/chapter4/firstsection/BreadFirstPaths.java
+++ src/main/java/algorithms/chapter4/firstsection/BreadFirstPaths.java
@@ -59,7 +59,7 @@ public class BreadFirstPaths {
 
 
 	public static void main(String[] args) {
-		Graph g = new Graph(6);
+		/*Graph g = new Graph(6);
 		g.addEdge(0,5);
 		g.addEdge(2,4);
 		g.addEdge(2,3);
@@ -67,8 +67,17 @@ public class BreadFirstPaths {
 		g.addEdge(0,1);
 		g.addEdge(3,4);
 		g.addEdge(3,5);
-		g.addEdge(0,2);
-		int start = 5;
+		g.addEdge(0,2);*/
+		Graph g = new Graph(8);
+		g.addEdge(0,1);
+		g.addEdge(1,2);
+		g.addEdge(2,3);
+		g.addEdge(3,4);
+		g.addEdge(4,5);
+		g.addEdge(5,6);
+		g.addEdge(6,7);
+		System.out.println(g);
+		int start = 0;
 		BreadFirstPaths s = new BreadFirstPaths(g, start);
 		for (int v = 0; v < g.getV(); v++) {
 			System.out.print(start + "  到 "+v+":");
