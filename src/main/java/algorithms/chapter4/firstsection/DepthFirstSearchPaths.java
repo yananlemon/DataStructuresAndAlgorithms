b1d1bb42b0c22fb34abe15cf23986b831d1a567d
aaron.yan <aaron.yan@analyticservice.net>
2018-06-14 15:12:39 +0800

    add image

--- src/main/java/algorithms/chapter4/firstsection/DepthFirstSearchPaths.java
+++ src/main/java/algorithms/chapter4/firstsection/DepthFirstSearchPaths.java
@@ -45,7 +45,7 @@ public class DepthFirstSearchPaths {
 	}
 
 	public static void main(String[] args) {
-		Graph g = new Graph(6);
+		/*Graph g = new Graph(6);
 		g.addEdge(0,5);
 		g.addEdge(2,4);
 		g.addEdge(2,3);
@@ -53,9 +53,22 @@ public class DepthFirstSearchPaths {
 		g.addEdge(0,1);
 		g.addEdge(3,4);
 		g.addEdge(3,5);
-		g.addEdge(0,2);
+		g.addEdge(0,2);*/
 		
-		DepthFirstSearchPaths s = new DepthFirstSearchPaths(g, 0);
+		Graph g = new Graph(8);
+		g.addEdge(0,1);
+		g.addEdge(0,2);
+		g.addEdge(1,3);
+		g.addEdge(3,2);
+		g.addEdge(3,4);
+		g.addEdge(3,5);
+		g.addEdge(4,5);
+		g.addEdge(4,6);
+		g.addEdge(5,6);
+		g.addEdge(5,7);
+		g.addEdge(6,7);
+		
+		DepthFirstSearchPaths s = new DepthFirstSearchPaths(g, 3);
 		for (int v = 0; v < g.getV(); v++) {
 			System.out.print("0 到 "+v+":");
 			if(s.hasPathTo(v)){
